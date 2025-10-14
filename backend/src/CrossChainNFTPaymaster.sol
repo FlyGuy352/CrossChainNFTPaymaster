69a1c0db8c1c244fa5c5ece0517381ecc8e2e27d
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 10:35:00 +0800

    pm

--- backend/src/CrossChainNFTPaymaster.sol
+++ backend/src/CrossChainNFTPaymaster.sol
@@ -3,23 +3,89 @@ pragma solidity ^0.8.30;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {PaymasterCore, PackedUserOperation} from "@openzeppelin/community-contracts/account/paymaster/PaymasterCore.sol";
+import "@openzeppelin/contracts/interfaces/draft-IERC4337.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract CrossChainNFTPaymaster is PaymasterCore, Ownable {
-    function extractData(bytes memory data) internal pure returns (bytes memory adminSignature, uint256 tokenId, address userAddress, bytes memory userSignature) {
-         if (data.length != 234) {
+
+    error ParseFailed();
+    error AdminSignatureFailed();
+    error UserSignatureFailed();
+
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
+    address private immutable _admin;
+
+    mapping(address user => mapping(uint256 tokenId => uint256 nonce)) public nonces;
+
+    constructor(address owner, address admin) Ownable(owner) {
+        _admin = admin;
+    }
+
+    function _validatePaymasterUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 maxCost
+    ) internal override returns (bytes memory context, uint256 validationData) {
+        (bytes memory adminSignature, uint256 tokenId, address userAddress, bytes memory userSignature) = extractData(userOp.paymasterAndData);
+        bytes32 adminHash = keccak256(abi.encodePacked(tokenId, userAddress));
+        if (adminHash.toEthSignedMessageHash().recover(adminSignature) != _admin) {
+            revert AdminSignatureFailed();
+        }
+        uint256 currentNonce = nonces[userAddress][tokenId];
+        bytes32 userHash = keccak256(abi.encodePacked(address(this), currentNonce));
+        if (userHash.toEthSignedMessageHash().recover(userSignature) != userAddress) {
+            revert UserSignatureFailed();
+        }
+        nonces[userAddress][tokenId]++;
+        return ("", 0);
+    }
+
+    function extractData(bytes memory data) internal pure returns (
+        bytes memory adminSignature,
+        uint256 tokenId,
+        address userAddress,
+        bytes memory userSignature
+    ) {
+        if (data.length != 234) {
             revert ParseFailed();
         }
-        
+
         adminSignature = new bytes(65);
         userSignature = new bytes(65);
-
+        uint256 paymasterDataOffset = 52;
+        
         assembly {
             let dataPtr := add(data, 32)
-            dataPtr := add(dataPtr, 52)
-            mcopy(add(0x20, adminSignature), dataPtr, 65)
-            tokenId := mload(add(dataPtr, 65))
-            userAddress := mload(add(dataPtr, 85))
-            mcopy(add(0x20, userSignature), add(dataPtr, 117), 65)
+            let adminSignaturePtr := add(adminSignature, 32)
+            let userSignaturePtr := add(userSignature, 32)
+
+            {
+                let src := add(dataPtr, paymasterDataOffset)
+                let dest := adminSignaturePtr
+                let len := 65
+                for { let i := 0 } lt(i, len) { i := add(i, 32) } {
+                    mstore(dest, mload(src))
+                    src := add(src, 32)
+                    dest := add(dest, 32)
+                }
+            }
+
+            tokenId := mload(add(dataPtr, 117))
+            userAddress := mload(add(dataPtr, 137))
+
+            {
+                let src := add(dataPtr, 169)
+                let dest := userSignaturePtr
+                let len := 65
+                for { let i := 0 } lt(i, len) { i := add(i, 32) } {
+                    mstore(dest, mload(src))
+                    src := add(src, 32)
+                    dest := add(dest, 32)
+                }
+            }
         }
     }
 }
