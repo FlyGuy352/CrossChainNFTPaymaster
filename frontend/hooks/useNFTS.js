ea13c39d57074dd49df0a817540cb9841fcd7f52
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 17:26:07 +0800

    useNFT hook

---
+++ frontend/hooks/useNFTS.js
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import { createPublicClient, http } from 'viem';
+import { localhost } from 'viem/chains';
+import contractAddresses from '@/constants/contractAddresses.json';
+import nftAbi from '@/constants/DummyNFT.json';
+
+export default function useNFTs(ownerAddress) {
+    const client = createPublicClient({
+        chain: localhost,
+        transport: http(),
+    });
+
+    return useQuery({
+        queryKey: ['nfts', ownerAddress],
+        queryFn: async () => {
+            const events = await client.getContractEvents({
+                address: contractAddresses.DummyNFT,
+                abi: nftAbi,
+                eventName: 'Transfer',
+                args: { to: ownerAddress }
+            });
+
+            const tokenIds = events.map(event => event.args.tokenId);
+            const tokenURIs = await Promise.all(tokenIds.map(tokenId =>
+                client.readContract({
+                    address: contractAddresses.DummyNFT,
+                    abi: nftAbi,
+                    functionName: 'tokenURI',
+                    args: [tokenId]
+                })
+            ));
+            return tokenIds.map((id, index) => ({ id, uri: tokenURIs[index] }));
+        },
+        enabled: Boolean(ownerAddress)
+    });
+}
