f69775cd54656444ebc77f103f86be5a8c3ec6f0
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 17:08:41 +0800

    ad hooks

--- frontend/actions/actions.js
+++ frontend/actions/actions.js
@@ -6,7 +6,14 @@ import networks from '@/constants/networks.json';
 import nftAbi from '@/constants/DummyNFT.json';
 
 export const signMint = async owner => {
+    const provider = new ethers.JsonRpcProvider(networks.NFTChain.rpcUrl, networks.NFTChain.id);
+    const nftContract = new ethers.Contract(contractAddresses.DummyNFT, nftAbi, provider);
+    const tokenId = await nftContract.latestTokenId();
 
+    const hash = ethers.solidityPackedKeccak256(['uint256', 'address'], [tokenId + BigInt(1), owner]);
+    const signature = await new ethers.Wallet(process.env.HEDERA_TESTNET_PRIVATE_KEY_ADMIN).signMessage(ethers.toBeArray(hash));
+    
+    return signature;
 };
 
 export const hashUserOp = async () => {
