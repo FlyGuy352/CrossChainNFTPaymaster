e0d5a661fc95c60523cf8dcd7e7c090b4ce28e69
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 08:25:23 +0800

    extractData

--- backend/src/CrossChainNFTPaymaster.sol
+++ backend/src/CrossChainNFTPaymaster.sol
@@ -2,6 +2,24 @@
 pragma solidity ^0.8.30;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {PaymasterCore} from "@openzeppelin/community-contracts/account/paymaster/PaymasterCore.sol";
+import {PaymasterCore, PackedUserOperation} from "@openzeppelin/community-contracts/account/paymaster/PaymasterCore.sol";
 
-contract CrossChainNFTPaymaster is PaymasterCore, Ownable {}
+contract CrossChainNFTPaymaster is PaymasterCore, Ownable {
+    function extractData(bytes memory data) internal pure returns (bytes memory adminSignature, uint256 tokenId, address userAddress, bytes memory userSignature) {
+         if (data.length != 234) {
+            revert ParseFailed();
+        }
+        
+        adminSignature = new bytes(65);
+        userSignature = new bytes(65);
+
+        assembly {
+            let dataPtr := add(data, 32)
+            dataPtr := add(dataPtr, 52)
+            mcopy(add(0x20, adminSignature), dataPtr, 65)
+            tokenId := mload(add(dataPtr, 65))
+            userAddress := mload(add(dataPtr, 85))
+            mcopy(add(0x20, userSignature), add(dataPtr, 117), 65)
+        }
+    }
+}
