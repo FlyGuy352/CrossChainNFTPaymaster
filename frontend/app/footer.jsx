a6d3c5c4f7aae3aa3d8db0bf0beaf156e604f397
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 16:15:19 +0800

    favicon

--- frontend/app/footer.jsx
+++ frontend/app/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer>
+        <footer className='sticky top-[100vh] flex justify-end p-2 text-xs bg-linear-to-br from-slate-700 to-slate-900 text-white'>
             Copyright &copy; 2025, Tom Lin
         </footer>
     );
--- frontend/app/header.jsx
+++ frontend/app/header.jsx
@@ -1,13 +1,29 @@
 import Link from 'next/link';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Image from 'next/image';
+import logoImage from '@/public/assets/images/Logo.png';
 
 export default function Header() {
     return (
-        <>
-        <div>
-            Cross-Chain<br/>NFT PayMaster
-        </div>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        </>
+      <div className='font-BDOGrotesk'>
+        <nav className='flex justify-between p-5'>
+          <Link className='flex gap-1' href='/'>
+            <Image
+                src={logoImage}
+                width={40}
+                height={40}
+                alt='Logo'
+            />
+            <div className='flex items-center font-bold text-sm leading-[0.8rem]'>
+              Cross-Chain<br/>NFT PayMaster
+            </div>
+          </Link>
+          <ConnectButton />
+        </nav>
+        <nav className='flex justify-center gap-72 py-20 font-bold text-[#C6BABA]'>
+          <Link href='/' className='hover:text-gray-800'>Home</Link>
+          <Link href='/about' className='hover:text-gray-800'>About</Link>
+        </nav>
+      </div>
     );
 };
