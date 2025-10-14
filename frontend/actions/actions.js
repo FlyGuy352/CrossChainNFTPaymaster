cc5cbcb73aa58e72b9c5022ab39977d976f213d0
FlyGuy352 <tom.lin@aodigy.com>
2025-10-14 21:06:53 +0800

    actions

--- frontend/actions/actions.js
+++ frontend/actions/actions.js
@@ -19,7 +19,52 @@ export const signMint = async owner => {
     return signature;
 };
 
-export const hashUserOp = async () => {
+export const hashUserOp = async (tokenId, userAddress, userSignature) => {
+    const transactionChainProvider = new ethers.JsonRpcProvider(
+        networks.TransactionChain.rpcUrl, networks.TransactionChain.id
+    );
+
+    const entryPoint = new ethers.Contract(contractAddresses.EntryPoint, entryPointAbi, transactionChainProvider);
+    const sender = contractAddresses.SmartContractWallet;
+    const simpleCounter = new ethers.Contract(contractAddresses.SimpleCounter, counterAbi);
+    const smartContractWallet = new ethers.Contract(contractAddresses.SmartContractWallet, walletAbi);
+    const callData = smartContractWallet.interface.encodeFunctionData(
+        'execute', [contractAddresses.SimpleCounter, 0, simpleCounter.interface.encodeFunctionData('increment')]
+    );
+
+    const verificationGasLimit = 200000;
+    const callGasLimit = 200000;
+    const verificationGasLimitBytes = ethers.zeroPadValue(ethers.hexlify(ethers.toBeArray(verificationGasLimit)), 16);
+    const callGasLimitLimitBytes = ethers.zeroPadValue(ethers.hexlify(ethers.toBeArray(callGasLimit)), 16);
+    const accountGasLimits = ethers.concat([verificationGasLimitBytes, callGasLimitLimitBytes]);
+
+    const maxPriorityFeePerGas = ethers.parseUnits('5', 'gwei');
+    const maxFeePerGas = ethers.parseUnits('10', 'gwei');
+    const maxPriorityFeePerGasBytes = ethers.zeroPadValue(ethers.hexlify(ethers.toBeArray(maxPriorityFeePerGas)), 16);
+    const maxFeePerGasBytes = ethers.zeroPadValue(ethers.hexlify(ethers.toBeArray(maxFeePerGas)), 16);
+    const gasFees = ethers.concat([maxPriorityFeePerGasBytes, maxFeePerGasBytes]);
+
+    const nftChainProvider = new ethers.JsonRpcProvider(networks.NFTChain.rpcUrl, Number(networks.NFTChain.id));
+    const nftContract = new ethers.Contract(contractAddresses.DummyNFT, nftAbi, nftChainProvider);
+    const adminSignature = await nftContract.signatures(tokenId);
+    const paymasterAndData = ethers.solidityPacked(
+        ['address', 'uint128', 'uint128', 'bytes', 'uint256', 'address', 'bytes'],
+        [contractAddresses.PayMaster, 200000, 200000, adminSignature, tokenId, userAddress, userSignature]
+    );
+    const userOp = {
+        sender,
+        nonce: (await entryPoint.getNonce(sender, 0)).toString(),
+        initCode: '0x',
+        callData,
+        accountGasLimits,
+        preVerificationGas: 50000,
+        gasFees,
+        paymasterAndData,
+        signature: '0x'
+    };
+    const userOpHash = await entryPoint.getUserOpHash(userOp);
+
+    return { userOp, userOpHash };
 };
 
 export const transmitUserOp = async userOp => {
